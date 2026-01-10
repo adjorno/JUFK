212f8e50d663540581b71844bf2b304ccd13fe3e
Mykhailo Dorokhin <adjorno@gmail.com>
2026-01-10 16:31:17 +0100

    Remove custom ktlint check

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,20 +37,6 @@ subprojects {
         }
     }
 
-    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
-        version.set("1.7.1")
-        android.set(true)
-        outputColorName.set("RED")
-        filter {
-            exclude { it.file.path.contains("/build/") }
-            exclude { it.file.path.contains("/generated/") }
-        }
-        reporters {
-            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
-            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
-        }
-    }
-
     configure<io.gitlab.arturbosch.detekt.extensions.DetektExtension> {
         buildUponDefaultConfig = true
         config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
